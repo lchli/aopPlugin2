42a764987ac63a9de5f377c274719c5f02f45a8b
simpleli <996863054@qq.com>
2022-05-10 11:37:32 +0800

    fix

--- plugin/src/main/java/com/lch/aop/plugin/ApmPluginExtension.java
+++ plugin/src/main/java/com/lch/aop/plugin/ApmPluginExtension.java
@@ -1,18 +1,12 @@
 package com.lch.aop.plugin;
 
-import org.gradle.api.provider.Property;
-
 import kotlin.jvm.functions.Function1;
 import kotlin.jvm.functions.Function3;
 
-/**
- * 作者：simpleli on 2022/5/9 00:29
- * 邮箱：lchli@mexc.com
- */
 public class ApmPluginExtension {
-    public boolean isOpenAop=true;
-    public boolean isAopAllProject=true;
-    public Function1<String,Boolean> isAopClass;
-    public Function3<String,String,String,Boolean> isAopMethod;
+    public boolean isOpenAop = true;
+    public boolean isAopAllProject = true;
+    public Function1<String, Boolean> isAopClass;
+    public Function3<String, String, String, Boolean> isAopMethod;
 
 }
